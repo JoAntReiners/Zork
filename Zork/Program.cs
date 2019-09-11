3389e2f6dae74f948db6712c17d555dd8d68f22d
WinembergJo <jwine@knights.ucf.edu>
2019-09-11 14:12:52 -0400

    Page 5 Completed
    Added Commands.cs (Sorry didn't notice it earlier)

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,19 +2,12 @@
 
 namespace Zork
 {
-    enum Commands
-    {
-        QUIT,
-        LOOK,
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST,
-        UNKNOWN
-    }
 
     class Program
     {
+        private static string[] Rooms = {"Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static int currentPos = 1;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -24,33 +17,32 @@ namespace Zork
             while(command != Commands.QUIT)
             {
                 string outputString = "";
-
+                Console.WriteLine(Rooms[currentPos]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
                 switch (command)
                 {
-                    case Commands.NORTH:
-                        outputString = "You moved NORTH";
-                        break;
+                    case Commands.NORTH:                      
                     case Commands.SOUTH:
-                        outputString = "You moved SOUTH";
-                        break;
                     case Commands.EAST:
-                        outputString = "You moved EAST";
-                        break;
                     case Commands.WEST:
-                        outputString = "You moved WEST";
+                        if (Move(command))
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
                         break;
                     case Commands.LOOK:
-                        outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
                     case Commands.QUIT:
                         outputString = "Thank you for playing!";
                         break;
                     case Commands.UNKNOWN:
-                        outputString = "Unknown Command.";
-                        break;
                     default:
                         outputString = "Unknown Command.";
                         break;
@@ -62,6 +54,56 @@ namespace Zork
         }
 
         private static Commands ToCommand(string commandString) => (Enum.TryParse(commandString, true, out Commands result)) ? result : Commands.UNKNOWN;
+
+        private static bool isCommand(Commands commandString)
+        {
+            bool isCommand = false;
+            switch (commandString)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                case Commands.EAST:
+                case Commands.WEST:
+                    isCommand = true;
+                    break;
+                default:
+                    isCommand = false;
+                    break;
+            }
+
+            return isCommand;
+        }
+
+        private static bool Move(Commands command)
+        {
+            if(isCommand(command))
+            {
+
+            }
+            else
+            {
+                throw new Exception("Not a valid input!");
+            }
+
+            bool moveSuccessful;
+
+            switch(command)
+            {
+                
+                case Commands.EAST when currentPos < Rooms.Length - 1:
+                    moveSuccessful = true;
+                    currentPos++;
+                    break;
+                case Commands.WEST when currentPos > 0:
+                    moveSuccessful = true;
+                    currentPos--;
+                    break;
+                default:
+                    moveSuccessful = false;
+                    break;
+            }
+            return moveSuccessful;
+        }
 
     }
 }
